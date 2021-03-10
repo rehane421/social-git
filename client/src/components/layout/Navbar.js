cd5f63441be6f1a2319c3291571c83e8daae3e7d
Ashish Roshan <rehane421@gmail.com>
2021-03-10 11:23:58 -0800

    Fontawesome CDN worked

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -37,7 +37,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
           <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
-      { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>) }
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
